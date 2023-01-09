0ad7b3beffe88f742c403b7e9d3c8e29638fe354
Babi Mumba <babimumba243@gmail.com>
2023-01-09 22:34:41 +0200

    add enw file

--- app/src/main/java/com/babimumba/e_learning/hm/NestedActivity.kt
+++ app/src/main/java/com/babimumba/e_learning/hm/NestedActivity.kt
@@ -3,11 +3,25 @@ package com.babimumba.e_learning.hm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.babimumba.e_learning.R
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.models.SlideModel
 
 class NestedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nested)
+
+        val imageList = ArrayList<SlideModel>() // Create image list
+
+// imageList.add(SlideModel("String Url" or R.drawable)
+// imageList.add(SlideModel("String Url" or R.drawable, "title") You can add title
+
+        imageList.add(SlideModel("https://bit.ly/2YoJ77H", "The animal population decreased by 58 percent in 42 years."))
+        imageList.add(SlideModel("https://bit.ly/2BteuF2", "Elephants and tigers may become extinct."))
+        imageList.add(SlideModel("https://bit.ly/3fLJf72", "And people do that."))
+
+        val imageSlider = findViewById<ImageSlider>(R.id.image_slider)
+        imageSlider.setImageList(imageList)
 
     }
 }
