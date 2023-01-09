a96eec80c768db21e9bdf473b0b68ee02041d506
Babi Mumba <babimumba243@gmail.com>
2023-01-09 22:27:31 +0200

    add enw file

---
+++ app/src/main/java/com/babimumba/e_learning/hm/NestedActivity.kt
@@ -0,0 +1,13 @@
+package com.babimumba.e_learning.hm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.babimumba.e_learning.R
+
+class NestedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nested)
+
+    }
+}
