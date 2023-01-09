474fad50a1f58b0d9404eeaec2bc40bde140b573
Babi Mumba <babimumba243@gmail.com>
2023-01-09 23:24:51 +0200

    ne commit -m

--- app/src/main/java/com/babimumba/e_learning/hm/NestedActivity.kt
+++ app/src/main/java/com/babimumba/e_learning/hm/NestedActivity.kt
@@ -2,8 +2,11 @@ package com.babimumba.e_learning.hm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.babimumba.e_learning.R
 import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
 import com.denzcoskun.imageslider.models.SlideModel
 
 class NestedActivity : AppCompatActivity() {
@@ -16,11 +19,21 @@ class NestedActivity : AppCompatActivity() {
 // imageList.add(SlideModel("String Url" or R.drawable)
 // imageList.add(SlideModel("String Url" or R.drawable, "title") You can add title
 
-        imageList.add(SlideModel("https://www.esisalama.com/assets/img/carousel/banner_principale.jpg", "Ecole superieur d'informatique salama"))
+        imageList.add(SlideModel("https://www.esisalama.com/assets/img/carousel/banner_principale.jpg", "Ecole superieur d'informatique salama",ScaleTypes.FIT))
         imageList.add(SlideModel("https://www.esisalama.com/assets/img/carousel/banner_genie_log.png", "reseau informatique"))
         imageList.add(SlideModel("https://www.esisalama.com/assets/img/carousel/banner_design.png", "DEsign est multimedia"))
 
         val imageSlider = findViewById<ImageSlider>(R.id.image_slider)
+        imageSlider.setItemClickListener(object : ItemClickListener {
+            override fun onItemSelected(position: Int) {
+                Toast.makeText(this@NestedActivity, "n $position", Toast.LENGTH_SHORT).show()
+            }
+        })
+        imageSlider.setItemClickListener(object : ItemClickListener {
+            override fun onItemSelected(position: Int) {
+                // You can listen here.
+            }
+        })
         imageSlider.setImageList(imageList)
 
     }
